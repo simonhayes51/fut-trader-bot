8435f4828265ee77f35ab082a6389a4325c57fb7
simonhayes51 <simonhayes_@hotmail.com>
2025-08-19 20:38:18 +0100

    Update taxcalc.py

--- cogs/taxcalc.py
+++ cogs/taxcalc.py
@@ -49,8 +49,5 @@ class TaxCalc(commands.Cog):
 
         await interaction.response.send_message(embed=embed)
 
-    async def cog_load(self):
-        self.tree.add_command(self.taxcalc)
-
 async def setup(bot):
     await bot.add_cog(TaxCalc(bot))
